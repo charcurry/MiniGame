5d0745cf57be1d0740a663c21e659ab875c7875d
Charlie <charlieryancurry@gmail.com>
2023-10-31 15:20:49 -0300

    make player

--- MiniGame/Program.cs
+++ MiniGame/Program.cs
@@ -13,9 +13,48 @@ namespace MiniGame
             Console.WriteLine("MiniGame");
             Console.WriteLine();
 
-            for (int i = 1; i <= 10; i++)
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            int y = 10;
+            int x = 10;
+            bool gameOver = false;
+            while (!gameOver)
             {
-                Console.WriteLine(i);
+                ConsoleKeyInfo input = Console.ReadKey();
+
+                if (input.Key == ConsoleKey.W)
+                {
+                    y--;
+                    if (y < 0) y = 0;
+                    Console.Clear();
+                }
+                else if (input.Key == ConsoleKey.A) 
+                { 
+                    x--;
+                    if (x < 0) x = 0;
+                    Console.Clear();
+                }
+                else if (input.Key == ConsoleKey.D)
+                {
+                    x++;
+                    Console.Clear();
+                }
+                else if (input.Key == ConsoleKey.S)
+                {
+                    y++;
+                    if (y > 100) y = 100;
+                    Console.Clear();
+                }
+
+                Console.SetCursorPosition(x, y);
+                // ..
+                // ..
+                Console.WriteLine("@");
+                // ..
+                // ..
             }
 
             Console.WriteLine();
