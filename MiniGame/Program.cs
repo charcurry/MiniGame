c448635f1e1e7e3b67662949d50f3373df878a1d
Charlie <charlieryancurry@gmail.com>
2023-10-31 15:56:45 -0300

    add quit button

--- MiniGame/Program.cs
+++ MiniGame/Program.cs
@@ -8,53 +8,59 @@ namespace MiniGame
 {
     internal class Program
     {
+        static int y = 10;
+        static int x = 10;
+        static bool gameOver = false;
+
+        static void PlayerDraw(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine("@");
+        }
+
+        static void PlayerUpdate()
+        {
+            ConsoleKeyInfo input = Console.ReadKey();
+
+            if (input.Key == ConsoleKey.W)
+            {
+                y--;
+                if (y < 0) y = 0;
+                Console.Clear();
+            }
+            else if (input.Key == ConsoleKey.A)
+            {
+                x--;
+                if (x < 0) x = 0;
+                Console.Clear();
+            }
+            else if (input.Key == ConsoleKey.D)
+            {
+                x++;
+                if (x > 50) x = 50;
+                Console.Clear();
+            }
+            else if (input.Key == ConsoleKey.S)
+            {
+                y++;
+                if (y > 50) y = 50;
+                Console.Clear();
+            }
+            else if (input.Key == ConsoleKey.Escape)
+            {
+                gameOver = true;
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("MiniGame");
             Console.WriteLine();
-
-            //for (int i = 1; i <= 10; i++)
-            //{
-            //    Console.WriteLine(i);
-            //}
-
-            int y = 10;
-            int x = 10;
-            bool gameOver = false;
+            
             while (!gameOver)
             {
-                ConsoleKeyInfo input = Console.ReadKey();
-
-                if (input.Key == ConsoleKey.W)
-                {
-                    y--;
-                    if (y < 0) y = 0;
-                    Console.Clear();
-                }
-                else if (input.Key == ConsoleKey.A) 
-                { 
-                    x--;
-                    if (x < 0) x = 0;
-                    Console.Clear();
-                }
-                else if (input.Key == ConsoleKey.D)
-                {
-                    x++;
-                    Console.Clear();
-                }
-                else if (input.Key == ConsoleKey.S)
-                {
-                    y++;
-                    if (y > 100) y = 100;
-                    Console.Clear();
-                }
-
-                Console.SetCursorPosition(x, y);
-                // ..
-                // ..
-                Console.WriteLine("@");
-                // ..
-                // ..
+                PlayerDraw(x, y);
+                PlayerUpdate();
             }
 
             Console.WriteLine();
